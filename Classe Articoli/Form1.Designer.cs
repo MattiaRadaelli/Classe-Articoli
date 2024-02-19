c8a229ba09906a280cb563f1029afb5f40817f2f
MattiaRadaelli <valterradaelli278@gmail.com>
2024-02-19 09:40:58 +0100

    Finito (Interfaccia)

--- Classe Articoli/Form1.Designer.cs
+++ Classe Articoli/Form1.Designer.cs
@@ -39,13 +39,25 @@
             this.label4 = new System.Windows.Forms.Label();
             this.prezzo = new System.Windows.Forms.TextBox();
             this.checkbox2 = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label11 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // checkbox
             // 
             this.checkbox.AutoSize = true;
-            this.checkbox.Location = new System.Drawing.Point(114, 51);
+            this.checkbox.Location = new System.Drawing.Point(373, 26);
             this.checkbox.Name = "checkbox";
             this.checkbox.Size = new System.Drawing.Size(66, 17);
             this.checkbox.TabIndex = 0;
@@ -54,9 +66,9 @@
             // 
             // aggiungi
             // 
-            this.aggiungi.Location = new System.Drawing.Point(11, 51);
+            this.aggiungi.Location = new System.Drawing.Point(371, 108);
             this.aggiungi.Name = "aggiungi";
-            this.aggiungi.Size = new System.Drawing.Size(97, 40);
+            this.aggiungi.Size = new System.Drawing.Size(170, 41);
             this.aggiungi.TabIndex = 1;
             this.aggiungi.Text = "Aggiungi";
             this.aggiungi.UseVisualStyleBackColor = true;
@@ -80,7 +92,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(359, 9);
+            this.label2.Location = new System.Drawing.Point(189, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(81, 13);
             this.label2.TabIndex = 4;
@@ -88,7 +100,7 @@
             // 
             // codice
             // 
-            this.codice.Location = new System.Drawing.Point(362, 25);
+            this.codice.Location = new System.Drawing.Point(192, 25);
             this.codice.Name = "codice";
             this.codice.Size = new System.Drawing.Size(173, 20);
             this.codice.TabIndex = 5;
@@ -96,7 +108,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(183, 9);
+            this.label3.Location = new System.Drawing.Point(8, 56);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(103, 13);
             this.label3.TabIndex = 6;
@@ -104,16 +116,15 @@
             // 
             // descrizione
             // 
-            this.descrizione.Location = new System.Drawing.Point(186, 25);
-            this.descrizione.Multiline = true;
+            this.descrizione.Location = new System.Drawing.Point(11, 72);
             this.descrizione.Name = "descrizione";
-            this.descrizione.Size = new System.Drawing.Size(170, 66);
+            this.descrizione.Size = new System.Drawing.Size(170, 20);
             this.descrizione.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(362, 55);
+            this.label4.Location = new System.Drawing.Point(189, 56);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(80, 13);
             this.label4.TabIndex = 8;
@@ -121,7 +132,7 @@
             // 
             // prezzo
             // 
-            this.prezzo.Location = new System.Drawing.Point(365, 71);
+            this.prezzo.Location = new System.Drawing.Point(192, 72);
             this.prezzo.Name = "prezzo";
             this.prezzo.Size = new System.Drawing.Size(170, 20);
             this.prezzo.TabIndex = 9;
@@ -129,28 +140,145 @@
             // checkbox2
             // 
             this.checkbox2.AutoSize = true;
-            this.checkbox2.Location = new System.Drawing.Point(114, 74);
+            this.checkbox2.Location = new System.Drawing.Point(445, 26);
             this.checkbox2.Name = "checkbox2";
-            this.checkbox2.Size = new System.Drawing.Size(66, 17);
+            this.checkbox2.Size = new System.Drawing.Size(69, 17);
             this.checkbox2.TabIndex = 10;
-            this.checkbox2.Text = "Prodotto";
+            this.checkbox2.Text = "Materiale";
             this.checkbox2.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // checkBox1
             // 
-            this.button1.Location = new System.Drawing.Point(11, 154);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(97, 26);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Aggiungi";
-            this.button1.UseVisualStyleBackColor = true;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(373, 74);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "In Possesso";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(370, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Carta Fedeltà:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Data Scadenza";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(11, 124);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(170, 20);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(189, 108);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "È Fresco?";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(192, 129);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(35, 17);
+            this.checkBox3.TabIndex = 18;
+            this.checkBox3.Text = "Si";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(250, 108);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 13);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Entro Quando (max 5):";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(251, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 20);
+            this.textBox1.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(8, 156);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(102, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Materiale Principale:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(11, 172);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 20);
+            this.textBox2.TabIndex = 22;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(189, 156);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(71, 13);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "È Riciclabile?";
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(192, 175);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(35, 17);
+            this.checkBox4.TabIndex = 24;
+            this.checkBox4.Text = "Si";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(370, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(42, 13);
+            this.label11.TabIndex = 25;
+            this.label11.Text = "Cos\'è?:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(760, 370);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.checkbox2);
             this.Controls.Add(this.prezzo);
             this.Controls.Add(this.label4);
@@ -183,7 +311,19 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox prezzo;
         private System.Windows.Forms.CheckBox checkbox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Label label11;
     }
 }
 
